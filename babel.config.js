a3648c3d81e8df7fc53947a7fb43ea4551b4f92b
suraj.suman.swain <suraj.suman.swain@accenture.com>
2023-04-06 16:32:14 +0530

    V6

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    presets:[
-        "@babel/preset-env",
-        "@babel/preset-react"
-    ]
-}
+    presets: [
+      [
+        "@babel/preset-react",
+        {
+          development: process.env.BABEL_ENV === "development",
+        },
+      ],
+    ],
+  };
